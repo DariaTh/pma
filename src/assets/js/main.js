cb4256cdbc0fa2ba5e1733303e97d39f0d6a4d41
DariaDo <dariathoruk@gmail.com>
2023-10-27 13:39:35 +0300

    add see what others section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -143,3 +143,21 @@ $(document).ready(function () {
   });
 
 });
+
+$('.testimonial-carousel').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
